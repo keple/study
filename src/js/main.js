3af7042e6c85656903d1931244e02ac3fc1eaf2c
keple <smw195@gmail.com>
2019-12-13 02:58:12 +0900

    add sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,108 @@
+var app = angular.module('app',['navigation','contents','menu'])
 
-console.log(angular);
-angular.module('navigation',[])
-.controller('navControll',function(){
+var nav = angular.module('navigation',[]);
+nav.controller('navControll',function(navService){
     this.test = 'tests';
-})
+    this.topMenus=[
+        {
+            iconClass:'fa fa-server fa-2x',
+            name:'Service'
+        },
+        {
+            iconClass:'fa fa-address-book fa-2x',
+            name:'Projects'
+        },
+        {
+            iconClass:'fa fa-bandcamp fa-2x',
+            name:'Test'
+        }
+    ]
+    this.goTo = function(){
 
+    }
+});
+
+nav.service('navService',function($http){
+   
+
+});
+
+var contents = angular.module('contents',['ngRoute']);
+
+contents.config(function($routeProvider){
+    $routeProvider.when('/',{
+        templateUrl:'/src/html/default.html'
+    })
+    .when('/setting',{
+        templateUrl:'/src/html/setting.html'
+    })
+    .when('/moniter',{
+        templateUrl:'/src/html/moniter.html'
+    })
+    .otherwise({redirectTo:'/'})
+});
+contents.controller('contentsCtrl',function(){
+
+});
+contents.service('contentsServ',function(){
+
+});
+
+var menu = angular.module('menu',['ui.router']);
+menu.config(function($stateProvider){
+    $stateProvider.state('/',{
+        url:'/',
+        templateUrl:'/src/html/default.html',
+        controller:'contentsCtrl'
+    })
+    .state('/setting',{
+        url:'/setting',
+        templateUrl:'/src/html/setting.html',
+        controller:'contentsCtrl'
+    })
+    .state('/moniter',{
+        url:'/moniter',
+        templateUrl:'/src/html/moniter.html',
+        controller:'contentsCtrl'
+    })
+
+});
+menu.controller('menuCtrl',function($state){
+    
+    this.menuList = [{
+            iconClass:'fa fa-home fa-2x',
+            name:'Summary',
+            tab:'/'
+        },
+        {
+            iconClass:'fa fa-cogs fa-2x',
+            name:'Setting',
+            tab:'/setting'
+        },
+        {
+            iconClass:'fa fa-television fa-2x',
+            name:'Moniter',
+            tab:'/moniter'
+        }
+    ]
+    this.actives = [];
+
+    this.movePage = function(tab){
+        console.log("in?",tab);
+        switch(tab){
+            case 0 : $state.go(this.menuList[0].tab);
+                console.log("in?",this.menuList[0].tab);
+                break;
+            case 1 : $state.go(this.menuList[1].tab);
+                break;
+            case 2 : $state.go(this.menuList[2].tab);
+                break;
+            default:$state.go('#/');
+                break;
+        }
+    }
+});
+
+menu.service('menuService',function(){
+
+});
