0b38f776a389fc5567f9288fbe2cc28ff693a498
keple <smw195@gmail.com>
2020-01-14 22:45:13 +0900

    setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   // enntry file
-  entry: ['./src/js/main.js','@babel/pollyfill'],
+  entry: './src/js/main.js',
   // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
   output: {
     path: path.resolve(__dirname, 'dist/js'),
@@ -28,5 +28,6 @@ module.exports = {
   },
   devtool: 'source-map',
   // https://webpack.js.org/concepts/mode/#mode-development
+  target:'web',
   mode: 'development'
 };
